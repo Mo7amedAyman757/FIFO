30af8cd66cb90c89e84435ca6840309b82ee2a19
Mo7amedAyman757 <ma5550236@gmail.com>
2026-05-15 22:41:40 +0300

    add FIFO architecture and depth calculation documentation

--- FIFO_uvm/FIFO_coverage.sv
+++ FIFO_uvm/FIFO_coverage.sv
@@ -22,13 +22,25 @@ import FIFO_sequence_item_pkg::*;
             almostempt_cvg : coverpoint cv_seq_item.almostempty;
             underflow_cvg : coverpoint cv_seq_item.underflow;
 
-            wr_ack_C: cross wr_cvg, rd_cvg , wr_ack_cvg;
-            overflow_C: cross wr_cvg, rd_cvg , overflow_cvg;
-            full_C: cross wr_cvg, rd_cvg , full_cvg;
+            wr_ack_C: cross wr_cvg, rd_cvg , wr_ack_cvg{
+                ignore_bins a0 = binsof(wr_cvg) intersect {0} && binsof(rd_cvg) intersect {0} && binsof(wr_ack_cvg) intersect {1};
+                ignore_bins a1 = binsof(wr_cvg) intersect {0} && binsof(rd_cvg) intersect {1} && binsof(wr_ack_cvg) intersect {1};
+            }
+            overflow_C: cross wr_cvg, rd_cvg , overflow_cvg{
+                ignore_bins b0 = binsof(wr_cvg) intersect {0} && binsof(rd_cvg) intersect {0} && binsof(overflow_cvg) intersect {1};
+                ignore_bins b1 = binsof(wr_cvg) intersect {0} && binsof(rd_cvg) intersect {1} && binsof(overflow_cvg) intersect {1};
+            }
+            full_C: cross wr_cvg, rd_cvg , full_cvg{
+                ignore_bins c0 = binsof(wr_cvg) intersect {1} && binsof(rd_cvg) intersect {1} && binsof(full_cvg) intersect {1};
+                ignore_bins c1 = binsof(wr_cvg) intersect {0} && binsof(rd_cvg) intersect {1} && binsof(full_cvg) intersect {1};
+            }
             empty_C: cross wr_cvg, rd_cvg , empty_cvg;
             almostfull_C: cross wr_cvg, rd_cvg , almostfull_cvg;
             almostempty_C: cross wr_cvg, rd_cvg , almostempt_cvg;
-            underflow_C: cross wr_cvg, rd_cvg , underflow_cvg;
+            underflow_C: cross wr_cvg, rd_cvg , underflow_cvg{
+                ignore_bins d0 = binsof(wr_cvg) intersect {1} && binsof(rd_cvg) intersect {0} && binsof(underflow_cvg) intersect {1};
+                ignore_bins d1 = binsof(wr_cvg) intersect {0} && binsof(rd_cvg) intersect {0} && binsof(underflow_cvg) intersect {1};
+            }
 
         endgroup
 
